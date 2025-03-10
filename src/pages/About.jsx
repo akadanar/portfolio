63503d42d571bdcf472bcf67594b37082d21c1e6
akadanar <irsyadsabil@proton.me>
2025-03-10 20:54:45 +0700

    Fix: Unresponsive component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,9 +4,9 @@ import Sns from '../components/Sns'
 export default function About(){
     return (
         <>
-        <div className="mx-72 my-32">
-            <span className="text-2xl font-bold">Hi! I'm Akadanar</span>
-            <p className='text-xl'>A senior high school student who have an interest in physics and computer science.</p>
+        <div className="mx-[12%] my-32">
+            <span className="sm:text-xl md:text-2xl font-bold">Hi! I'm Akadanar</span>
+            <p className="sm:text-lg md:text-xl">A senior high school student who have an interest in physics and computer science.</p>
         </div>
         <Panel/>
         <Sns/>
